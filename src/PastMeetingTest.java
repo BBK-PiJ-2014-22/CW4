3ee184fad152631e9f445c766801190dd121179c
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-03-03 19:57:09 +0000

    PastMeetingTest - get notes - written

--- src/PastMeetingTest.java
+++ src/PastMeetingTest.java
@@ -20,13 +20,15 @@ public class PastMeetingTest {
 	Calendar meetingdate;
 	Set<Contact> contacts;
 	int meetingid;
-	Meeting meeting;
+	PastMeeting meeting;
+	String notes;
 	
 	/**Parameters should be the class of meeting. Parameters should be:
 	 * 
 	 * 1) Meeting ID
 	 * 2) Time (in months) before or after current date that the meeting date should be set to
 	 * 3) Number of contacts to add to the meeting
+	 * 4) Notes (null for no notes passed)
 	 * 
 	 * @return the parameters for testing
 	 */
@@ -34,19 +36,22 @@ public class PastMeetingTest {
 	@Parameters
 	public static Collection<Object[]> data(){
 		return Arrays.asList(new Object[][] {     
-                {1,  1,  0 },
-                {2, -1,  1 },
-                {3,  0 , 2 }
+                {1,  -1,  0, null },
+                {1,  -1,  1, "" },
+                {1,  -1,  1, "Past Meeting Notes" },
+                {1,  1,   1, "Future Meeting Notes" }
+
           });
 		
 	}
 	
-	public PastMeetingTest(int id, int monthChange, int numberOfContacts){
+	public PastMeetingTest(int id, int monthChange, int numberOfContacts, String notes){
 		
 		this.meetingdate = new GregorianCalendar();
 		this.meetingdate.add(Calendar.MONTH,monthChange);
 		this.meetingid = id;
 		this.contacts = new HashSet<Contact>();
+		this.notes = notes;
 		
 		for (int i = 0 ; i < numberOfContacts ; i++)
 			this.contacts.add(new ContactImpl(i, "Contact"+i, "Notes for contact "+i));
@@ -54,9 +59,16 @@ public class PastMeetingTest {
 	
 	@Before
 	public void setUp(){
-		meeting = new MeetingImpl(this.meetingid, this.meetingdate, this.contacts);
+		this.meeting = new PastMeetingImpl(this.meetingid, this.meetingdate, this.contacts);
 	}
-
-
 	
+	@Test
+	public void testGetContacts(){
+		
+		String comparator = this.notes;
+		if (comparator == null)
+			comparator = "";
+		assertEquals(comparator, this.meeting.getNotes());
+			
+	}
 }
