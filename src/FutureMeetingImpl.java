86bebdb325d9a3fcd9366e61a34662fefa1b5a52
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-03-08 19:19:41 +0000

    FutureMeetingImpl - contructor modified
    now throws IlegalArgumentException for dates in past or future

--- src/FutureMeetingImpl.java
+++ src/FutureMeetingImpl.java
@@ -1,13 +1,15 @@
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Set;
 
 
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+	
+	//TODO - add constructor exceptions, copyconstructo
 
-	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts){
-		
-		//TODO - add constructor exceptions, copyconstructo
-
+	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts){	
 		super(id, date, contacts);
-		}
+		if (date.compareTo(new GregorianCalendar()) <= 0 )
+			throw new IllegalArgumentException();
+	}
 }
