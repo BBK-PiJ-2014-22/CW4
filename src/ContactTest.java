e6c3b41aef51127664476780020a4152bba9a65e
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2015-03-21 14:52:18 +0000

    Simplified ContactTest (Removed Parameters)
    ContactTest was not really using it's paramterization so this has been
    removed to simplify the code

--- src/ContactTest.java
+++ src/ContactTest.java
@@ -1,35 +1,26 @@
 
 
 import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
+/**UnitTests for ContactImpl
+ * 
+ * @author Jamie
+ *
+ */
 public class ContactTest {
 	
 	Contact contact;
 	int targetID;
 	String targetName;
 	String targetNotes;
+
 	
-	@Parameters
-	public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {     
-                 { 1, "Bob Geldoff", "First Contact"}     
-           });
-    }
-	
-	public ContactTest(int id, String name, String notes){
-		this.targetID = id;
-		this.targetName = name;
-		this.targetNotes = notes;
+	public ContactTest(){
+		this.targetID = 1;
+		this.targetName = "Bob Geldoff";
+		this.targetNotes = "First Contact";
 	}
 	
 	@Before
